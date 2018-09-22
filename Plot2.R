4e81a588da274847ad3203f3ebc1a515832b3f2b
David Robinson <41377456+djrobbo83@users.noreply.github.com>
2018-09-22 14:07:16 +0100

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,56 @@
+#EXPLORATORY DATA ANALYSIS: COURSE PROJECT 2
+
+#Fine particulate matter (PM2.5) is an ambient air pollutant for which there is strong evidence that it is harmful to human health.
+#In the United States, the Environmental Protection Agency (EPA) is tasked with setting national ambient air quality standards for 
+#fine PM and for tracking the emissions of this pollutant into the atmosphere. Approximatly every 3 years, the EPA releases its 
+#database on emissions of PM2.5. This database is known as the National Emissions Inventory (NEI). 
+#You can read more information about the NEI at the EPA National Emissions Inventory web site.
+
+#For each year and for each type of PM source, the NEI records how many tons of PM2.5 were emitted from that source over the course
+#of the entire year. The data that you will use for this assignment are for 1999, 2002, 2005, and 2008.
+
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+#
+#               1. INSTALL REQUIRED PACKAGES & CALL LIBRARIES
+#                  SET WORKING DIRECTORY
+#
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+#SET YOUR WORKING DIRECTORY HERE; THIS IS WHERE THE .RDS FILES SHOULD BE DOWNLOADED TO FROM
+# DOWNLOAD DATA FROM LINK
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
+# UNZIP AND PLACE summarySCC_PM25.rds & Source_Classification_Code.rds IN THE WORKING DIRECTORY
+setwd("C:\\Data Science Specialization JHU\\C4_Exploratory_Data_Analysis\\Project2")
+
+
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+#
+#               2. READ IN DATA USING readRDS() FUNCTION
+#
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#head(NEI)
+#head(SCC)
+
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+#
+#               Q2. Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510"
+#                   from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+#
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+baltimore_MD <- subset(NEI, NEI$fips == "24510")
+balt_ann_emissions <- with(baltimore_MD, tapply(Emissions, year, sum))
+#NICE COLOURS
+rb <- colorRampPalette(c("Light Blue", "Blue"))
+dat_rb <- rb(4)[cut(balt_ann_emissions, breaks = 4)]
+#PLOT
+png("plot2.png", width = 480, height = 480)
+barplot(balt_ann_emissions, 
+        xlab = "year", 
+        ylab = expression("Total PM"[2.5]*" Emitted (ton)"), 
+        main = "Baltimore State: Total Emissions By Year",
+        col  = dat_rb)
+dev.off()
